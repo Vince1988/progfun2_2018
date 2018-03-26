f69f54a3338a2dc0038a38c7039523e8e5abf825
Vincent Genecand <vincent.genecand@gmail.com>
2018-03-26 21:59:41 +0200

    Add ScalaCheck properties for heap testing | week 3

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,4 +19,65 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("findMin") = forAll { (a: Int, b: Int) =>
+    val min = a min b
+    findMin(insert(a, insert(b, empty))) == min
+  }
+
+  property("deleteMin1") = forAll { a : Int =>
+    deleteMin(insert(a, empty)) == empty
+  }
+
+  property("deleteMinMultiple") = forAll { l: List[Int] =>
+    val list = l.distinct
+    list.size <= 2 || {
+      val heap = list.foldLeft(empty)((h, i) => insert(i, h))
+      findMin(deleteMin(heap)) != list.min
+    }
+  }
+
+  property("deleteMinIsSorted") = forAll { h: H =>
+    val list = toList(h)
+    list == list.sorted
+  }
+
+  property("findMinAfterMeld") = forAll { (h1: H, h2: H) =>
+    val min = List(h1, h2).filterNot(isEmpty).map(findMin)
+    if (min.isEmpty) true
+    else min contains findMin(meld(h1, h2))
+  }
+
+  property("size") = forAll { h: H =>
+    size(h) == toList(h).size
+  }
+
+  property("meldSize") = forAll { (h1: H, h2: H) =>
+    val m = meld(h1, h2)
+    size(h1) + size(h2) == size(m)
+  }
+
+  property("allInsertsPresent") = forAll { (a: Int, b: Int) =>
+    val l = toList(insert(b, insert(a, empty)))
+    l.contains(a) && l.contains(b)
+  }
+
+  def size(heap: H): Int = {
+    def go(h: H, s: Int): Int =
+    {
+      if (isEmpty(h)) s
+      else go(deleteMin(h), s + 1)
+    }
+    go(heap, 0)
+  }
+
+  def toList(heap: H): List[A] = {
+    def go(h: H, xs: List[A]): List[A] = {
+      if (isEmpty(h))
+        xs.reverse
+      else
+        go(deleteMin(h), findMin(h) :: xs)
+    }
+    go(heap, List())
+  }
+
 }
